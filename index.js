8f71d25798a659d0a20baf2cff91ed9623be4a3c
saurabhlm10 <saurabhlm10@gmail.com>
2024-02-17 18:19:44 +0530

    added ability to handle GET routes

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const MyExpress = require("./src/myExpress");
 
 const app = new MyExpress();
 
+app.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Home Page");
+});
+
+app.get("/about", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("About Page");
+});
+
 app.listen(3000, () => {
   console.log("MyExpress server running on http://localhost:3000");
 });
--- src/myExpress.js
+++ src/myExpress.js
@@ -2,10 +2,26 @@ const http = require("http");
 
 class MyExpress {
   constructor() {
+    this.routes = [];
+    console.log("routes", this.routes);
     this.server = http.createServer((req, res) => {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Hello from MyExpress\n");
+      const matchingRoute = this.routes.find(
+        (route) =>
+          route.path === req.url && route.method === req.method.toLowerCase()
+      );
+
+      if (matchingRoute) {
+        matchingRoute.handler(req, res);
+      } else {
+        res.writeHead(404);
+        res.end("Not Found");
+      }
     });
+  }
+
+  get(path, handler) {
+    this.routes.push({ path, method: "get", handler });
+    console.log("routes", this.routes);
   }
 
   listen(port, callback) {
