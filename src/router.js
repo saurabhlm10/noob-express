188ea1c4cbe0aff0acb87a0aad989fe42ba63124
saurabhlm10 <saurabhlm10@gmail.com>
2024-02-18 00:20:55 +0530

    Update router.js for Route Parameters: "Enhanced Router to support route parameters, allowing dynamic route matching and parameter extraction."

--- src/router.js
+++ src/router.js
@@ -4,21 +4,51 @@ class Router {
   }
 
   get(path, handler) {
-    this.routes.push({ path, method: "get", handler });
+    const route = {
+      path: this.parsePath(path),
+      handler,
+      keys: [],
+    };
+
+    route.path = path.replace(/:([^\/]+)/g, (match, key) => {
+      route.keys.push(key);
+      return "([^\\/]+)";
+    });
+
+    route.regex = new RegExp(`^${route.path}$`);
+
+    console.log(route.regex);
+    this.routes.push(route);
   }
 
   handle(req, res) {
-    const route = this.routes.find(
-      (route) =>
-        route.path === req.url && route.method === req.method.toLowerCase()
-    );
-
-    if (route) {
-      route.handler(req, res);
-    } else {
-      res.writeHead(404);
-      res.end("Not Found");
+    const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+    for (const route of this.routes) {
+      const match = pathname.match(route.regex);
+      console.log("match", match);
+      if (match && req.method.toLowerCase() === "get") {
+        const params = this.extractParams(route, match);
+        req.params = params;
+        route.handler(req, res);
+        return;
+      }
     }
+    res.writeHead(404);
+    res.end("Not Found");
+  }
+
+  parsePath(path) {
+    return path.replace(/:([^\/]+)/g, "([^\\/]+)");
+  }
+
+  extractParams(route, match) {
+    const params = {};
+    route.keys.forEach((key, index) => {
+      params[key] = match[index + 1];
+    });
+
+    console.log(params);
+    return params;
   }
 }
 
